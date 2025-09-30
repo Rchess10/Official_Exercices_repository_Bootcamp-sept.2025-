53c51f6ea44b34cd702ad077b6b10c3e4a2ee1e2
Rchess10 <romain.vautey@gmail.com>
2025-09-30 14:51:09 +0200

    mon premier commit

---
+++ Exercice.py
@@ -0,0 +1,27 @@
+print("hello")
+print("world")
+print("!")
+
+user_name = input("What is your name?")
+age = input("How old are you? ")
+
+print("Hello " + user_name + ", you are " + age + " years old.")
+
+#1. Write a Python expression to calculate the sum of 10 and 3.
+print (10 +3)
+#2. Subtract 7 from 15 using Python.
+print(15 - 7)
+#3. Multiply 4 by 8 using Python.
+print(4 * 8)
+#4. Divide 18 by 3.
+print(18 / 3)
+#5. Use floor division to find how many whole times 11 can be divided by 4.
+print(11 // 4)
+#6. Find the remainder when 9 is divided by 4 using the modulus operator.
+print(9 % 4)
+#7. Raise 6 to the power of 3.
+print(6 ** 3)
+#8. Use the negation operator to make the number 12 negative.
+print(-12)
+
+
