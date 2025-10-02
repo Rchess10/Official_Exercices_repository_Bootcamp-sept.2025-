9d92b0cf4e2b387ec96c6048ca2030abd9995335
Rchess10 <romain.vautey@gmail.com>
2025-10-02 13:36:58 +0200

    dernier push

---
+++ Python_Data_Types.py
@@ -0,0 +1,37 @@
+x = 3 
+y = "dogs" 
+print (str(x)+ " " + y)
+
+num = 1999
+favorite_number = 7
+birth_place = "Maisons Laffites"
+birth_date = "08/08/1999"
+print("I was born in " + str(num) + " in " + birth_place + " on " + birth_date + " and my favorite number is " + str(favorite_number) + ".")
+
+my_string = "Python"
+start = 3
+end = 6
+print(my_string[0])  
+print(my_string[-2]) 
+print(my_string[start:end])
+
+str_num = "50"
+print(int(str_num) + 25)
+
+num = 1
+print(bool(num))
+
+string = "hello"
+print(list(string))  # Output: ['h', 'e', 'l', 'l', 'o']
+
+pairs = [("name", "John"), ("age", 25)]
+d = dict(pairs)
+print(d["age"])  # Affiche: 25
+
+new_string = "J" + my_string[0:]
+print (new_string)  # Output: Jython
+
+name_new = "Alice"
+age_new = 30 
+greeting = f"my name is {name_new} and I am {age_new} years old."
+print(greeting)
---
+++ lists.py
@@ -0,0 +1,12 @@
+fruit = ("banana", "apple", "orange" )
+print (fruit [-1]) #print the second fruit of the list
+
+
+number = {101, 102, 103, 101}
+number_micro = list(number)
+print(number_micro[-1]) # Affiche le dernier élément de la liste
+
+number_set = {5, 10, 15, 20}
+number_list = list(number_set)
+print("Last element:", number_list[-0])  # Print the last index of the list converted from set
+
